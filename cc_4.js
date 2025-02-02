3bd173b458c00a40f7e6eb647c2e068f93cf9e9c
Julia <lorrayne@usf.edu>
2025-02-02 18:26:41 -0500

    Task 1 - Customer Discounts.

---
+++ cc_4.js
@@ -0,0 +1,10 @@
+//Task 1: If Statements
+let purchaseAmount = 200; //Declared variable purchaseAmount with a value
+
+if (purchaseAmount >100) {
+    finalAmount = purchaseAmount-(purchaseAmount*0.1)
+} else {
+    finalAmount = purchaseAmount
+}; //Used an if statement to apply a discount if the amount is greater than $100
+
+console.log(`Final amount after discount: $${finalAmount}`); //Logged the final amount to the console 
