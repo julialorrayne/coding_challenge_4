e61c984cf0f92bbf0df0688c23600ca29756fb08
Julia <lorrayne@usf.edu>
2025-02-02 19:32:39 -0500

    Task 3 - Inventory Depletion.

--- cc_4.js
+++ cc_4.js
@@ -9,6 +9,7 @@ if (purchaseAmount >100) {
 
 console.log(`Final amount after discount: $${finalAmount}`); //Logged the final amount to the console
 
+
 //Task 2: For Loop
 let sales = [100,150,200,250,300]; //Declare an array sales with five sales figures
 sum = 0;
@@ -17,3 +18,12 @@ for(let i=0; i<sales.length;i++){
 }; //Used a for loop to calculate the total sales
 
 console.log(`Total sales amount: $${sum}`); //Logged the total sales to the console
+
+
+//Task 3: While Loop
+let stock = 10; //Declared a variable stock with an initial value of 10
+while (stock>0) {
+    console.log(`Stock value is $${stock}.`) //Logged each decrement to the console using a template literal
+    stock--;   
+}; //Used a while loop to decrease stock until it reaches zero.
+
