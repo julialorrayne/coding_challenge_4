87288acdca3843fe6f26ab39514a6f0bc18b2893
Julia <lorrayne@usf.edu>
2025-02-02 19:23:47 -0500

    Task 2 - Sales Report.

--- cc_4.js
+++ cc_4.js
@@ -7,4 +7,13 @@ if (purchaseAmount >100) {
     finalAmount = purchaseAmount
 }; //Used an if statement to apply a discount if the amount is greater than $100
 
-console.log(`Final amount after discount: $${finalAmount}`); //Logged the final amount to the console 
+console.log(`Final amount after discount: $${finalAmount}`); //Logged the final amount to the console
+
+//Task 2: For Loop
+let sales = [100,150,200,250,300]; //Declare an array sales with five sales figures
+sum = 0;
+for(let i=0; i<sales.length;i++){
+    sum += sales[i]
+}; //Used a for loop to calculate the total sales
+
+console.log(`Total sales amount: $${sum}`); //Logged the total sales to the console
